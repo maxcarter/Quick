c5d45da2e6ae73b205f7d8c4d236ab709eee9830
maxcarter <max4carter@gmail.com>
2015-11-21 21:13:26 -0500

    Start of NodeJS Router

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
 	res.send('Hello World');
 });
+
+app.use('/api', require('./api/api'));
 
 app.listen(3000);
 console.log("Hello World");
