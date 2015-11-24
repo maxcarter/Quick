ce7749d34bc575eccc2a38340280886ed7c83a94
maxcarter <max4carter@gmail.com>
2015-11-23 21:00:34 -0500

    Strict users schema

--- server/api/v1/models/users.js
+++ server/api/v1/models/users.js
@@ -2,11 +2,28 @@ var restful = require('node-restful');
 var mongoose = restful.mongoose;
 
 var schema = new mongoose.Schema({
-    username: String,
-    password: String,
-    email: String,
-    displayName: String,
-    accessLevel: Number
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    displayName: {
+        type: String,
+        required: true,
+        default: "User"
+    },
+    accessLevel: {
+        type: Number,
+        required: true,
+        default: 2
+    }
 });
 
 module.exports = restful.model('Users', schema);
