d6b71379b546efaa07fa02fbc27d888cc178204b
maxcarter <max4carter@gmail.com>
2015-11-27 00:26:13 -0500

    Tasks Pagination

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -8,19 +8,49 @@
  * Controller of the quickApp
  */
 angular.module('quickApp')
-    .controller('SearchCtrl', function($scope, $alert, $routeParams, Request, config) {
+    .controller('SearchCtrl', function($scope, $alert, $routeParams, $location, Request, config) {
         this.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
 
+        $scope.jump = function(position, size, direction) {
+            switch (direction) {
+                case "fwd":
+                    var spot = position + size - 1;
+                    $location.search("startAt", spot + 1);
+                    $location.search("skip", spot);
+                    break;
+                case "bkwd":
+                    var spot = position - size;
+                    if (spot >= 0) {
+                        $location.search("startAt", spot);
+                        $location.search("skip", spot - 1);
+                    } else {
+                        $location.search("startAt", null);
+                        $location.search("skip", null);
+                    }
+                    break;
+            }
+        }
+
         $scope.request = Request;
         $scope.config = config;
         $scope.url = config.host + ":" + config.port + config.api;
         $scope.type = $routeParams.type;
 
-        $scope.request.get($scope.url + "/" + $scope.type).then(
+        $scope.queryParams = $location.search();
+        $scope.startAt = ($scope.queryParams.startAt) ? parseInt($scope.queryParams.startAt) : 1;
+        $scope.params = {
+            limit: ($scope.queryParams.limit) ? parseInt($scope.queryParams.limit) : 25,
+            skip: ($scope.queryParams.skip) ? parseInt($scope.queryParams.skip) : 0,
+            sort: ($scope.queryParams.sort) ? $scope.queryParams.sort : "dateCreated",
+        };
+        if ($scope.startAt) {
+            $scope.params.skip = $scope.startAt - 1;
+        }
+        $scope.request.get($scope.url + "/" + $scope.type, $scope.params).then(
             function(response) {
                 switch ($scope.type) {
                     case "tasks":
