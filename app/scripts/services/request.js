3f93c0c27c303ae852ef78ae1a1a6cef3b827044
maxcarter <max4carter@gmail.com>
2015-11-26 22:48:35 -0500

    Modified request server to log each request

--- app/scripts/services/request.js
+++ app/scripts/services/request.js
@@ -12,18 +12,33 @@ angular.module('quickApp')
         return {
             request: function(method, url, params, data) {
                 var deferred = $q.defer();
-                $http({
+                var request = {
                     method: method,
                     url: url,
                     params: params,
                     data: data
-                }).then(function successCallback(response) {
+                };
+                $http(request).then(function success(response) {
                     // this callback will be called asynchronously
                     // when the response is available
+                    console.log({
+                        url: url,
+                        success: true,
+                        method: method,
+                        request: request,
+                        response: response
+                    });
                     deferred.resolve(response);
-                }, function errorCallback(response) {
+                }, function error(response) {
                     // called asynchronously if an error occurs
                     // or server returns response with an error status.
+                    console.log({
+                        url: url,
+                        success: false,
+                        method: method,
+                        request: request,
+                        response: response
+                    });
                     deferred.reject(response);
                 });
                 return deferred.promise;
