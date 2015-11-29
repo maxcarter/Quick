058b095e2982bea61d3f80eee5b305775aa2537b
maxcarter <max4carter@gmail.com>
2015-11-29 01:12:46 -0500

    JSON web token validation added to the API

--- server/api/v1/routes/routes.js
+++ server/api/v1/routes/routes.js
@@ -4,29 +4,49 @@ var security = require('../security/security');
 module.exports = function(router) {
     var Users = require('../models/users');
     Users.methods(['get', 'put', 'post', 'delete']);
+    Users.before('get', security.check_token);
+    Users.before('put', security.check_token);
+    Users.before('post', security.check_token);
+    Users.before('delete', security.check_token);    
     Users.before('post', security.hash_password);
     Users.before('put', security.hash_password);
     Users.route('authenticate', {
         detail: false,
         handler: function(req, res, next) {
-            Users.findOne({
-                username: req.body.username
-            }, function(err, user) {
-                if (err) {
-                    throw err;
-                }
-                var result = security.authenticate_user(req, user);
-          		res.status(result.status).json(result);
-            });
+            if (req.method === "POST") {
+                Users.findOne({
+                    username: req.body.username
+                }, function(err, user) {
+                    if (err) {
+                        throw err;
+                    }
+                    var result = security.authenticate_user(req, user);
+                    res.status(result.status).json(result);
+                });
+            } else {
+            	res.status(405).json({
+                    status: 405,
+                    success: false,
+                    message: "Invalid request method! Expected POST"
+                });
+            }
         }
     });
     Users.register(router, '/users');
 
     var Tickets = require('../models/tickets');
     Tickets.methods(['get', 'put', 'post', 'delete']);
+    Tickets.before('get', security.check_token);
+    Tickets.before('put', security.check_token);
+    Tickets.before('post', security.check_token);
+    Tickets.before('delete', security.check_token);
     Tickets.register(router, '/tickets');
 
     var Sprints = require('../models/sprints');
     Sprints.methods(['get', 'put', 'post', 'delete']);
+    Sprints.before('get', security.check_token);
+    Sprints.before('put', security.check_token);
+    Sprints.before('post', security.check_token);
+    Sprints.before('delete', security.check_token);
     Sprints.register(router, '/sprints');
 }
--- server/api/v1/security/security.js
+++ server/api/v1/security/security.js
@@ -50,6 +50,28 @@ module.exports = {
             }
 
         }
+    },
+    check_token: function(req, res, next) {
+        var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
+        if (token) {
+            jwt.verify(token, secret, function(err, decoded) {
+                if (err) {
+                    return res.status(401).json({
+                        success: false,
+                        message: "Failed to authenticate token."
+                    });
+                } else {
+                    req.decoded = decoded;
+                    next();
+                }
+            });
+
+        } else {
+            return res.status(403).json({
+                success: false,
+                message: 'No token provided.'
+            });
+        }
     }
 };
