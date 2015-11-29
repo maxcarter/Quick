58d0ddfa5761e30adfef15446011eff2222d35bb
maxcarter <max4carter@gmail.com>
2015-11-28 22:32:48 -0500

    Added morgan request loging to server

--- server/server.js
+++ server/server.js
@@ -2,17 +2,23 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var path = require('path');
+var morgan = require('morgan');
+var jwt = require('jsonwebtoken');
 
+var config = require('./config');
 var app = express();
+var port = 3000;
+
+mongoose.connect(config.database);
+app.set('superSecret', config.secret);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-mongoose.connect('mongodb://localhost/quick');
-
+app.use(morgan('dev'));
 app.use(express.static('../dist'));
 app.use('/dev', express.static('../app')); 
 
 require('./api/api')(app);
 
-app.listen(3000);
+app.listen(port);
 console.log("Hello World");
