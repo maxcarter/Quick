e02d19bd2adee22abe3b48b325480b0958769c7f
maxcarter <max4carter@gmail.com>
2015-11-29 22:45:43 -0500

    Refactored login logic into a factory

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,61 +8,11 @@
  * Controller of the quickApp
  */
 angular.module('quickApp')
-    .controller('LoginCtrl', function($scope, $alert, $location, $cookies, $rootScope, Request, config) {
+    .controller('LoginCtrl', function($scope, $alert, Login, config) {
         this.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
-
-        $scope.url = config.host + ":" + config.port + config.api;
-        $scope.authenticate = function(username, password) {
-            if (username === null || username === "" || username === undefined) {
-                $alert({
-                    title: "Validation Error",
-                    content: "Username is a required field!",
-                    placement: 'top',
-                    type: 'danger',
-                    show: true,
-                    container: '#alert-container'
-                });
-                return;
-            }
-            if (password === null || password === "" || password === undefined) {
-                $alert({
-                    title: "Validation Error",
-                    content: "Password is a required field!",
-                    placement: 'top',
-                    type: 'danger',
-                    show: true,
-                    container: '#alert-container'
-                });
-                return;
-            }
-            var data = {
-                username: username,
-                password: password
-            };
-            Request.post($scope.url + "/users/authenticate", data).then(
-                function success(response) {
-                    config.username = username;
-                    config.token = response.data.token;
-                    $cookies.putObject('quickApp', {
-                        username: username,
-                        token: response.data.token
-                    });
-                    $rootScope.$broadcast("login-done");
-                    $location.url("/dashboard");
-                },
-                function error(response) {
-                    $alert({
-                        title: response.status + " " + response.statusText,
-                        content: response.data.message,
-                        placement: 'top',
-                        type: 'danger',
-                        show: true,
-                        container: '#alert-container'
-                    });
-                });
-        };
+        $scope.loginService = Login;
     });
---
+++ app/scripts/services/login.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name quickApp.Login
+ * @description
+ * # Login
+ * Factory in the quickApp.
+ */
+angular.module('quickApp')
+    .factory('Login', function($location, $rootScope, $alert, $cookies, Request, config) {
+        return {
+            url: config.host + ":" + config.port + config.api,
+            authenticate: function(username, password) {
+                if (username === null || username === "" || username === undefined) {
+                    $alert({
+                        title: "Validation Error",
+                        content: "Username is a required field!",
+                        placement: 'top',
+                        type: 'danger',
+                        show: true,
+                        container: '#alert-container'
+                    });
+                    return;
+                }
+                if (password === null || password === "" || password === undefined) {
+                    $alert({
+                        title: "Validation Error",
+                        content: "Password is a required field!",
+                        placement: 'top',
+                        type: 'danger',
+                        show: true,
+                        container: '#alert-container'
+                    });
+                    return;
+                }
+                var data = {
+                    username: username,
+                    password: password
+                };
+                Request.post(this.url + "/users/authenticate", data).then(
+                    function success(response) {
+                        config.username = username;
+                        config.token = response.data.token;
+                        $cookies.putObject('quickApp', {
+                            username: username,
+                            token: response.data.token
+                        });
+                        $rootScope.$broadcast("login-done");
+                        $location.url("/dashboard");
+                    },
+                    function error(response) {
+                        $alert({
+                            title: response.status + " " + response.statusText,
+                            content: response.data.message,
+                            placement: 'top',
+                            type: 'danger',
+                            show: true,
+                            container: '#alert-container'
+                        });
+                    });
+            }
+        };
+    });
