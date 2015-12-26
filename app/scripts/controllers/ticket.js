59ee1421a6eb60c21d520379a38712aad0fb0ec8
maxcarter <max4carter@gmail.com>
2015-12-26 18:37:52 -0500

    Progress control buttons

--- app/scripts/controllers/ticket.js
+++ app/scripts/controllers/ticket.js
@@ -8,28 +8,20 @@
  * Controller of the quickApp
  */
 angular.module('quickApp')
-    .controller('TicketCtrl', function($scope, $alert, $routeParams, Request, Time, config) {
+    .controller('TicketCtrl', function($scope, $alert, $route, $routeParams, Request, Time, Banner, config, Ticket) {
         this.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
 
+        $scope.ticketFactory = Ticket;
         $scope.time = Time;
         $scope.id = $routeParams.id;
         $scope.url = config.host + ":" + config.port + config.api;
         Request.get($scope.url + "/tickets/" + $scope.id).then(
             function success(response) {
-            	$scope._ticket = response.data;
-            },
-            function error(response) {
-                $alert({
-                    title: response.status + " " + response.statusText,
-                    content: response.data,
-                    placement: 'top',
-                    type: 'danger',
-                    show: true,
-                    container: '#alert-container'
-                });
-            });
+                $scope._ticket = response.data;
+            }, Banner.error);
+
     });
