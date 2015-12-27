21d80bad15ae227b7eef05e65d1de4698fdca82c
maxcarter <max4carter@gmail.com>
2015-12-27 14:28:16 -0500

    Api factory

---
+++ app/scripts/services/api.js
@@ -0,0 +1,255 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name quickApp.Api
+ * @description
+ * # Api
+ * Factory in the quickApp.
+ */
+angular.module('quickApp')
+    .factory('Api', function($q, Request, config) {
+        var host = config.host + ":" + config.port + config.api;
+        return {
+            tickets: {
+                endpoint: "/tickets",
+                get: function(id, params) {
+                    var deferred = $q.defer();
+                    var url = host + this.endpoint + "/" + id;
+                    params = (params) ? params : {};
+                    Request.get(url).then(
+                        function success(response) {
+                            deferred.resolve(response);
+                        },
+                        function error(response) {
+                            deferred.reject(response);
+                        });
+                    return deferred.promise;
+                },
+                put: function(id, data) {
+                    var deferred = $q.defer();
+                    var url = host + this.endpoint + "/" + id;
+                    data = (data) ? data : {};
+                    Request.put(url, data).then(
+                        function success(response) {
+                            deferred.resolve(response);
+                        },
+                        function error(response) {
+                            deferred.reject(response);
+                        });
+                    return deferred.promise;
+                },
+                post: function(id, data, params) {
+                    var deferred = $q.defer();
+                    var url = host + this.endpoint;
+                    data = (data) ? data : {};
+                    params = (params) ? params : {};
+                    Request.post(url, data, params).then(
+                        function success(response) {
+                            deferred.resolve(response);
+                        },
+                        function error(response) {
+                            deferred.reject(response);
+                        });
+                    return deferred.promise;
+                },
+                delete: function(id, params) {
+                    var deferred = $q.defer();
+                    var url = host + this.endpoint + "/" + id;
+                    params = (params) ? params : {};
+                    Request.delete(url).then(
+                        function success(response) {
+                            deferred.resolve(response);
+                        },
+                        function error(response) {
+                            deferred.reject(response);
+                        });
+                    return deferred.promise;
+                }
+            },
+            projects: {
+                endpoint: "/projects",
+                get: function(id, params) {
+                    var deferred = $q.defer();
+                    var url = host + this.endpoint + "/" + id;
+                    params = (params) ? params : {};
+                    Request.get(url).then(
+                        function success(response) {
+                            deferred.resolve(response);
+                        },
+                        function error(response) {
+                            deferred.reject(response);
+                        });
+                    return deferred.promise;
+                },
+                put: function(id, data) {
+                    var deferred = $q.defer();
+                    var url = host + this.endpoint + "/" + id;
+                    data = (data) ? data : {};
+                    Request.put(url, data).then(
+                        function success(response) {
+                            deferred.resolve(response);
+                        },
+                        function error(response) {
+                            deferred.reject(response);
+                        });
+                    return deferred.promise;
+                },
+                post: function(id, data, params) {
+                    var deferred = $q.defer();
+                    var url = host + this.endpoint;
+                    data = (data) ? data : {};
+                    params = (params) ? params : {};
+                    Request.post(url, data, params).then(
+                        function success(response) {
+                            deferred.resolve(response);
+                        },
+                        function error(response) {
+                            deferred.reject(response);
+                        });
+                    return deferred.promise;
+                },
+                delete: function(id, params) {
+                    var deferred = $q.defer();
+                    var url = host + this.endpoint + "/" + id;
+                    params = (params) ? params : {};
+                    Request.delete(url).then(
+                        function success(response) {
+                            deferred.resolve(response);
+                        },
+                        function error(response) {
+                            deferred.reject(response);
+                        });
+                    return deferred.promise;
+                }
+            },
+            sprints: {
+                endpoint: "/sprints",
+                get: function(id, params) {
+                    var deferred = $q.defer();
+                    var url = host + this.endpoint + "/" + id;
+                    params = (params) ? params : {};
+                    Request.get(url).then(
+                        function success(response) {
+                            deferred.resolve(response);
+                        },
+                        function error(response) {
+                            deferred.reject(response);
+                        });
+                    return deferred.promise;
+                },
+                put: function(id, data) {
+                    var deferred = $q.defer();
+                    var url = host + this.endpoint + "/" + id;
+                    data = (data) ? data : {};
+                    Request.put(url, data).then(
+                        function success(response) {
+                            deferred.resolve(response);
+                        },
+                        function error(response) {
+                            deferred.reject(response);
+                        });
+                    return deferred.promise;
+                },
+                post: function(id, data, params) {
+                    var deferred = $q.defer();
+                    var url = host + this.endpoint;
+                    data = (data) ? data : {};
+                    params = (params) ? params : {};
+                    Request.post(url, data, params).then(
+                        function success(response) {
+                            deferred.resolve(response);
+                        },
+                        function error(response) {
+                            deferred.reject(response);
+                        });
+                    return deferred.promise;
+                },
+                delete: function(id, params) {
+                    var deferred = $q.defer();
+                    var url = host + this.endpoint + "/" + id;
+                    params = (params) ? params : {};
+                    Request.delete(url).then(
+                        function success(response) {
+                            deferred.resolve(response);
+                        },
+                        function error(response) {
+                            deferred.reject(response);
+                        });
+                    return deferred.promise;
+                }
+            },
+            users: {
+                endpoint: "/users",
+                authenticate: function(username, password) {
+                    var deferred = $q.defer();
+                    var url = host + this.endpoint + "/authenticate";
+                    var data = {
+                        username: username,
+                        password: password
+                    };
+                    Request.post(url, data).then(
+                        function success(response) {
+                            deferred.resolve(response);
+                        },
+                        function error(response) {
+                            deferred.reject(response);
+                        });
+                    return deferred.promise;
+                },
+                get: function(id, params) {
+                    var deferred = $q.defer();
+                    var url = host + this.endpoint + "/" + id;
+                    params = (params) ? params : {};
+                    Request.get(url).then(
+                        function success(response) {
+                            deferred.resolve(response);
+                        },
+                        function error(response) {
+                            deferred.reject(response);
+                        });
+                    return deferred.promise;
+                },
+                put: function(id, data) {
+                    var deferred = $q.defer();
+                    var url = host + this.endpoint + "/" + id;
+                    data = (data) ? data : {};
+                    Request.put(url, data).then(
+                        function success(response) {
+                            deferred.resolve(response);
+                        },
+                        function error(response) {
+                            deferred.reject(response);
+                        });
+                    return deferred.promise;
+                },
+                post: function(id, data, params) {
+                    var deferred = $q.defer();
+                    var url = host + this.endpoint;
+                    data = (data) ? data : {};
+                    params = (params) ? params : {};
+                    Request.post(url, data, params).then(
+                        function success(response) {
+                            deferred.resolve(response);
+                        },
+                        function error(response) {
+                            deferred.reject(response);
+                        });
+                    return deferred.promise;
+                },
+                delete: function(id, params) {
+                    var deferred = $q.defer();
+                    var url = host + this.endpoint + "/" + id;
+                    params = (params) ? params : {};
+                    Request.delete(url).then(
+                        function success(response) {
+                            deferred.resolve(response);
+                        },
+                        function error(response) {
+                            deferred.reject(response);
+                        });
+                    return deferred.promise;
+                }
+            }
+        };
+    });
